ac7aac5b24a5f43ebed4303fb8f3f51860026b99
Ioan Fagarasan <raptor_dev@yahoo.com>
2019-02-28 14:25:09 +0000

    Added first view

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -1,3 +1,7 @@
+const Tech = require('../models/tech.model');
+
 exports.index = function (req, res) {
-    res.render('../views/pages/index');
+    Tech.find({}).then(techItems => {
+        res.render('../views/pages/index', {techItems: techItems});
+    });
 };
